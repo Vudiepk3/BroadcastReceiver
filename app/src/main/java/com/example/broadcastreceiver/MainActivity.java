42add5708036ef57a8614271d89b16dd893ffb6f
Vudiepk3 <110141392+Vudiepk3@users.noreply.github.com>
2025-02-22 16:34:15 +0700

    BroadcastReceiver trong Android Part 2 (Dynamic Receivers)

--- app/src/main/java/com/example/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/example/broadcastreceiver/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.broadcastreceiver;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 
 import com.example.broadcastreceiver.databinding.ActivityMainBinding;
@@ -7,10 +9,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private MyBroadcastReceiver myBroadcastReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter  intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(myBroadcastReceiver, intentFilter);
+    }
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(myBroadcastReceiver);
     }
 }
